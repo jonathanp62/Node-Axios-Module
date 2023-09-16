243f4b4c51c2214fff32f6074aae62bae0df0ce1
Jonathan Parker <joparker@penguinrandomhouse.com>
2023-09-16 12:03:08 -0400

    Initial content

---
+++ axios-module/src/axios-module.mjs
@@ -0,0 +1,24 @@
+/**
+ * (#)axios-module.mjs  1.0.0   09/16/2023
+ *
+ * Copyright (c) Jonathan M. Parker
+ * 324 Lantana Drive
+ * Owings Mills, MD  21117 U.S.A
+ * All Rights Reserved.
+ */
+
+//import { ProcessModule } from "./process-module.mjs";
+
+/**
+ * The application class.
+ */
+class AxiosModule {
+    /**
+     * The run method.
+     */
+    invokeAxios() {
+        console.log("Good afternoon");
+    }
+}
+
+export { AxiosModule };
